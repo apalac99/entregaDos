bf92b15447fd224308c23c0e7ca49553e3083dc4
Alejandro Palacios <apalac99@gmail.com>
2024-08-11 17:11:55 -0400

    todo

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 
-// no finalizado demo
-
-
 class Producto {
     static contador = 0; // Variable estática para contar las instancias
   
-    constructor(nombre, referencia, precio) {
+    constructor(nombre, referencia, precio, imagen) {
       this.nombre = nombre;
       this.referencia = referencia;
       this.precio = precio;
+      this.imagen = imagen;
       Producto.contador++; // Incrementar el contador al crear una nueva instancia
     }
   }
   
   // Crear productos
   const productos = [
-    new Producto("Producto 1", "REF001", 10),
-    new Producto("Producto 2", "REF002", 20),
-    new Producto("Producto 3", "REF003", 30),
-    new Producto("Producto 4", "REF004", 40),
-    new Producto("Producto 5", "REF005", 50),
-    new Producto("Producto 6", "REF006", 60)
+    new Producto("Patito verde", "REF001", 10, "1.jpg"),
+    new Producto("Patito mora", "REF002", 20, "2.jpg"),
+    new Producto("Patito azul", "REF003", 30, "3.jpg"),
+    new Producto("Patito rojo", "REF004", 40, "4.jpg"),
+    new Producto("Patito limon", "REF005", 50, "5.jpg"),
+    new Producto("Patito gris", "REF006", 60, "6.jpg")
   ];
   
   // Mostrar productos en el HTML
   for (let i = 0; i < productos.length; i++) {
     const producto = productos[i];
     const productElement = document.getElementById(`product${i + 1}`);
-    productElement.innerHTML = `${producto.nombre} - ${producto.referencia} - $${producto.precio} <button onclick="agregarAlCarrito(${producto.precio}, productos[${i}])">Agregar al carrito</button>`;
+    productElement.innerHTML = `<H2>${producto.nombre}</h2>  <img src="images/${producto.imagen}"  width="100" height="100"> <br>  ${producto.referencia} <br> $${producto.precio} <br><br><button onclick="agregarAlCarrito(${producto.precio}, productos[${i}])">Agregar al carrito</button>`;
   }
 
 const botonVaciar = document.getElementById("vaciarCarritoBtn");
